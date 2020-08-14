0357cf63d8850ca49c262a6e9500ff6c28c3c26c
Marian Nociar <majo.nociar@gmail.com>
2020-08-14 16:07:36 +0200

    mongodb-mongoose getting data from DB

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog')
 const app = express();
+
+// connect to mongoDB
+const dbURI = 'mongodb+srv://netninja:test1234@nodecluster.mjkee.mongodb.net/node-tuts?retryWrites=true&w=majority';
+// mongoose.connect(dbURI);
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(result => app.listen(3000))
+.catch(err => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 //app.set('views', 'myviews');
-
-app.listen(3000, () => {
-    console.log("resver running on port 3000");
-});
 
 // middleware & static files
 app.use('/public' ,express.static('public'));
@@ -21,6 +26,43 @@ app.use((req, res, next) => {
     next();
 });
 
+
+app.get('/add-blog', (req, res) => {
+    const blog = new Blog({
+        title: 'New blog',
+        snippet: 'About my new blog',
+        body: 'Lorem ipsum'
+    });
+
+    blog.save()
+    .then((result) => {
+        res.send(result);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+});
+
+app.get('/all-blogs', (req, res) => {
+    Blog.find()
+    .then(response => {
+        res.send(response);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+
+app.get('/single-blog', (req, res) => {
+    Blog.findById('5f36965eef5daf58bed7fb48')
+    .then( (result) => {
+        res.send(result);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'title 1', snippet: 'Lorem ipsum dolor, sit amet consectuter.'},
@@ -28,6 +70,14 @@ app.get('/', (req, res) => {
         {title: 'title 3', snippet: 'Lorem ipsum dolor, sit amet consectuter.'}
     ];
     res.render('index',{ title: 'Home', blogs});
+});
+
+app.get('/blogs', (req, res) => {
+    Blog.find().sort({createdAt: -1})
+    .then((result) => {
+        res.render('index', {title: 'Blogs', blogs: result})
+    })
+    .catch( err => console.log(err));
 });
 
 app.get('/about', (req, res) => {
