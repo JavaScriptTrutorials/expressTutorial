3541ca5fa7242a327d3bbddcf3f3023db3060fea
Marian Nociar <majo.nociar@gmail.com>
2020-08-14 11:13:30 +0200

    View Engines & passing simple data to templates

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+//app.set('views', 'myviews');
+
 app.listen(3000, () => {
     console.log("resver running on port 3000");
 });
 
 app.get('/', (req, res) => {
-    res.sendFile('./views/index.html', {root: __dirname});
+    res.render('index',{ title: 'Home'});
 });
 
 app.get('/about', (req, res) => {
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
+});
+
+app.get('/blogs/create', (req, res) => {
+    res.render('create',{ title: 'Create a new blog'});
 });
 
 // redirects
@@ -19,5 +27,5 @@ app.get('/about-us', (req, res) => {
 });
 
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', {root: __dirname});
+    res.status(404).render('404',{ title: '404'});
 });
