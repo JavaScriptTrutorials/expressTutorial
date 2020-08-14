bb15ff59c7cbd0275fd1e3f09f5ebe6d8f08c5c5
Marian Nociar <majo.nociar@gmail.com>
2020-08-14 14:54:54 +0200

    middlewares & static files

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 ### Lesson1
 Basic app handling some requests
+
+### Lesson2
+View engine (ejs), passing parameters to template
+
+### Lesson3
+Middleware and static files
+Middleware - code which runs (on the server) between getting requests and sending response
--- app.js
+++ app.js
@@ -9,6 +9,18 @@ app.listen(3000, () => {
     console.log("resver running on port 3000");
 });
 
+// middleware & static files
+app.use('/public' ,express.static('public'));
+
+app.use((req, res, next) => {
+    console.log('LOG MIDDLEWARE');
+    console.log('new request made: ');
+    console.log('host: ', req.hostname);
+    console.log('path: ', req.path);
+    console.log('method: ', req.method);
+    next();
+});
+
 app.get('/', (req, res) => {
     const blogs = [
         {title: 'title 1', snippet: 'Lorem ipsum dolor, sit amet consectuter.'},
